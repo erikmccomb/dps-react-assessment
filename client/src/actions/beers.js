9ff1bcb4e145838920b7264ab136101dfc0f57ec
Erik McComb <erik@erikmccomb.com>
2017-10-01 10:43:48 -0600

    removed code not needed

--- client/src/actions/beers.js
+++ client/src/actions/beers.js
@@ -8,24 +8,3 @@ export const getBeers = (number) => {
       .then(res => dispatch({ type: 'BEERS', beers: res.data.entries }))
   }
 }
-
-// export const addBeer = (beer) => {
-//   return (dispatch) => {
-//     axios.post('/api/all_beers', { beer })
-//       .then(res => dispatch({ type: 'ADD_BEER', Beer: res.data }))
-//   }
-// }
-
-// export const updateBeer = (beer) => {
-//   return (dispatch) => {
-//     axios.put(`/api/all_beers/${beer.id}`, { beer })
-//       .then(res => dispatch({ type: 'UPDATE_BEER', Beer: res.data }))
-//   }
-// }
-
-// export const deleteBeer = (id) => {
-//   return (dispatch) => {
-//     axios.delete(`/api/all_beers/${id}`)
-//       .then(() => dispatch({ type: 'DELETE_BEER', id }))
-//   }
-// }
