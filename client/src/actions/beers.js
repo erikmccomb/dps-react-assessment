d8ab0799b2fece91aeea610ba31a58ec246a7fbd
Erik McComb <erik@erikmccomb.com>
2017-10-01 10:56:08 -0600

    removed pagination stuff cus i have no idea what im doing.

--- client/src/actions/beers.js
+++ client/src/actions/beers.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // pagination notes
 // (number) is passed from BeerList.js "(getBeers(10))"   10 = number
-export const getBeers = (number) => {
+export const getBeers = () => {
   return (dispatch) => {
     axios.get('/api/all_beers')
       .then(res => dispatch({ type: 'BEERS', beers: res.data.entries }))
